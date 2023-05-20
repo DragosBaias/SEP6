05d16213d3b8cdec987f4dc7012aff90e0975a77
sackboynick <nicola.santolini@live.com>
2023-05-20 15:29:02 +0200

    Login

--- SEP6/Startup.cs
+++ SEP6/Startup.cs
@@ -1,8 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using SEP6.Data;
 
 namespace SEP6
 {
@@ -15,16 +22,15 @@ namespace SEP6
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
+            services.AddSingleton<WeatherForecastService>();
             services.AddScoped<TMDBApiClient>(provider => new TMDBApiClient("7f4f7ef07e4add825a6a5cedbbf03857"));
+            services.AddAuthorizationCore();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
