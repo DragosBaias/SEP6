d4835881c373563604adf6fff02bf6aaacf6db69
DragosBaias <dragosbaias@yahoo.com>
2023-05-20 15:40:05 +0200

    merged

--- SEP6/obj/Debug/net5.0/RazorDeclaration/Shared/NavMenu.razor.g.cs
+++ SEP6/obj/Debug/net5.0/RazorDeclaration/Shared/NavMenu.razor.g.cs
@@ -13,77 +13,77 @@ namespace SEP6.Shared
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\nicol\RiderProjects\SEP6\SEP6\_Imports.razor"
+#line 1 "C:\Users\Dragos\RiderProjects\SEP6\SEP6\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\nicol\RiderProjects\SEP6\SEP6\_Imports.razor"
+#line 2 "C:\Users\Dragos\RiderProjects\SEP6\SEP6\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\nicol\RiderProjects\SEP6\SEP6\_Imports.razor"
+#line 3 "C:\Users\Dragos\RiderProjects\SEP6\SEP6\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\nicol\RiderProjects\SEP6\SEP6\_Imports.razor"
+#line 4 "C:\Users\Dragos\RiderProjects\SEP6\SEP6\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\nicol\RiderProjects\SEP6\SEP6\_Imports.razor"
+#line 6 "C:\Users\Dragos\RiderProjects\SEP6\SEP6\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\nicol\RiderProjects\SEP6\SEP6\_Imports.razor"
+#line 7 "C:\Users\Dragos\RiderProjects\SEP6\SEP6\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\nicol\RiderProjects\SEP6\SEP6\_Imports.razor"
+#line 8 "C:\Users\Dragos\RiderProjects\SEP6\SEP6\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\nicol\RiderProjects\SEP6\SEP6\_Imports.razor"
+#line 9 "C:\Users\Dragos\RiderProjects\SEP6\SEP6\_Imports.razor"
 using SEP6;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\nicol\RiderProjects\SEP6\SEP6\_Imports.razor"
+#line 10 "C:\Users\Dragos\RiderProjects\SEP6\SEP6\_Imports.razor"
 using SEP6.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 11 "C:\Users\nicol\RiderProjects\SEP6\SEP6\_Imports.razor"
+#line 11 "C:\Users\Dragos\RiderProjects\SEP6\SEP6\_Imports.razor"
 using Microsoft.AspNetCore.Authentication;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 12 "C:\Users\nicol\RiderProjects\SEP6\SEP6\_Imports.razor"
+#line 12 "C:\Users\Dragos\RiderProjects\SEP6\SEP6\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
@@ -97,7 +97,7 @@ using Microsoft.AspNetCore.Components.Routing;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 29 "C:\Users\nicol\RiderProjects\SEP6\SEP6\Shared\NavMenu.razor"
+#line 29 "C:\Users\Dragos\RiderProjects\SEP6\SEP6\Shared\NavMenu.razor"
        
     private bool collapseNavMenu = true;
 
