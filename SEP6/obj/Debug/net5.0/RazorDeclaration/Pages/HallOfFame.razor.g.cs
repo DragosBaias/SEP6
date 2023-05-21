ce3822522cb256a1457b4dd0e70ec167f94d58ec
DragosBaias <dragosbaias@yahoo.com>
2023-05-21 11:36:48 +0200

    added HallOfFame page and code behind it incl. API calls

--- SEP6/obj/Debug/net5.0/RazorDeclaration/Pages/HallOfFame.razor.g.cs
+++ SEP6/obj/Debug/net5.0/RazorDeclaration/Pages/HallOfFame.razor.g.cs
@@ -90,14 +90,14 @@ using Microsoft.AspNetCore.Components.Routing;
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\Dragos\RiderProjects\SEP6\SEP6\Pages\MovieList.razor"
+#line 2 "C:\Users\Dragos\RiderProjects\SEP6\SEP6\Pages\HallOfFame.razor"
 using Entities;
 
 #line default
 #line hidden
 #nullable disable
-    [global::Microsoft.AspNetCore.Components.RouteAttribute("/MovieList")]
-    public partial class MovieList : global::Microsoft.AspNetCore.Components.ComponentBase
+    [global::Microsoft.AspNetCore.Components.RouteAttribute("/HallOfFame")]
+    public partial class HallOfFame : global::Microsoft.AspNetCore.Components.ComponentBase
     {
         #pragma warning disable 1998
         protected override void BuildRenderTree(global::Microsoft.AspNetCore.Components.Rendering.RenderTreeBuilder __builder)
@@ -105,85 +105,24 @@ using Entities;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 229 "C:\Users\Dragos\RiderProjects\SEP6\SEP6\Pages\MovieList.razor"
+#line 196 "C:\Users\Dragos\RiderProjects\SEP6\SEP6\Pages\HallOfFame.razor"
        
     private List<MovieDetails> _movies = new List<MovieDetails>();
-    private int _currentPage = 1;
-    private string _searchQuery = string.Empty;
-    private bool _searchMode = false;
     TMDBApiClient _tmdbApiClient;
-
+    
+    
     protected override async Task OnInitializedAsync()
     {
         _tmdbApiClient = new TMDBApiClient("7f4f7ef07e4add825a6a5cedbbf03857");
         await LoadMovies();
     }
-
+    
     private async Task LoadMovies()
     {
         
-        if (string.IsNullOrEmpty(_searchQuery))
-        {
-            _movies = await _tmdbApiClient.GetMovies(_currentPage);
-        }
-        else
-        {
-            List<MovieDetails> moviesByTitle = new List<MovieDetails>();
-            moviesByTitle.Add(await _tmdbApiClient.GetMovieByTitle(_searchQuery));
-            _movies = moviesByTitle;
-        }
-    }
-
-    private async Task ChangePage(int offset)
-    {
-        _currentPage += offset;
-        if (_currentPage < 1)
-            _currentPage = 1;
-
-        await LoadMovies();
-    }
-
-    private async Task SearchMovies()
-    {
-        _currentPage = 1;
-        await LoadMovies();
-        _searchMode = true;
-    }
-
-    private async void CloseSearch()
-    {
-        _searchMode = false;
-        _searchQuery = string.Empty;
-        _currentPage = 1;
-        await LoadMovies();
-        StateHasChanged();
+        _movies = await _tmdbApiClient.GetHoFMovie();
     }
     
-    private async Task FilterByDecade(ChangeEventArgs e)
-    {
-        if (e.Value != null)
-        {
-            string decade = e.Value.ToString();
-            if (decade.Equals("all"))
-                _movies = await _tmdbApiClient.GetPopularMovies();
-            else
-            {
-                _movies = await _tmdbApiClient.Get10MostPopularMoviesByDecade(int.Parse(decade.Replace("s","")));
-            }
-        }
-        StateHasChanged();
-    }
-
-    private async Task ViewMostPopularMovies()
-    {
-        _movies = await _tmdbApiClient.GetPopularMovies();
-        StateHasChanged();
-    }
-
-    private void ToggleSearch()
-    {
-        _searchMode = true;
-    }
 
 #line default
 #line hidden
