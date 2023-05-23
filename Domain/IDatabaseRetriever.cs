28ee5757957c501eb01c99c208327f982f3d6ca2
Adi-Vaitis <adriangabriel344@gmail.com>
2023-05-23 18:32:54 +0200

    Fixed the issue with the null id

--- Domain/IDatabaseRetriever.cs
+++ Domain/IDatabaseRetriever.cs
@@ -12,8 +12,8 @@ namespace Domain
         public Task<Movie> GetMovie(int id);
 
         public Task<MovieList> GetMovieList(int movieListId);
-        public void AddMovie(int movieListId, int movieId);
-        public void RemoveMovie(int movieListId, int movieId);
+        public Task<MovieList> AddMovie(int movieListId, int movieId);
+        public Task<MovieList> RemoveMovie(int movieListId, int movieId);
         
 
 
